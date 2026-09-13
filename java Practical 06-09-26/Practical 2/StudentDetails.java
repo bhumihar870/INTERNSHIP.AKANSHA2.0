e1dda896133cd10bec570c295c6a8bcdf8e8f596
Shubham Sharma <67571382+bhumihar870@users.noreply.github.com>
2026-09-13 15:34:11 +0530

    Update student details with new information

--- java Practical 06-09-26/Practical 2/StudentDetails.java
+++ java Practical 06-09-26/Practical 2/StudentDetails.java
@@ -1,8 +1,8 @@
 public class StudentDetails {
     public static void main(String[] args) {
-        String Studentname = "Dhanish Kumar";
-        int rollNo = 56;
-        int age = 24;
+        String Studentname = "Shubham sharma";
+        int rollNo = 57;
+        int age = 22;
 
         System.out.println("Student Name: " + Studentname);
         System.out.println("Student rollNo: " + rollNo);
