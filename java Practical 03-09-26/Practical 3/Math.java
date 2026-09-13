0c1709d0c8c26bef12aa5007601ca03c38d077a2
Shubham Sharma <67571382+bhumihar870@users.noreply.github.com>
2026-09-13 15:29:31 +0530

    Update values in Math class for addition

--- java Practical 03-09-26/Practical 3/Math.java
+++ java Practical 03-09-26/Practical 3/Math.java
@@ -1,8 +1,8 @@
 public class Math {
 
     public void values(){
-        int val1 = 24;
-        int val2 = 46;
+        int val1 = 44;
+        int val2 = 66;
         
         int result = val1 + val2;
         System.out.println("sum of " + val1 + " and " + val2 + " is:" + result );
